fdc4cbe63f7613a7d3a3426d15d33c0523e093e4
V Abhijith <119759593+VAbhijith2003github@users.noreply.github.com>
2023-10-28 02:23:29 +0530

    fix:login page created with post request to backend api endpoint

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import Homepage from "./components/pagescomponent/homepage";
+import Login from "./components/pagescomponent/login";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
-    </div>
-  );
+  <>
+  <Router>
+  <Routes>
+    <Route exact path = "/" element={<Homepage/>}/>
+    <Route exact path = "/login" element={<Login/>}/>
+    {/* <Route exact path = "/register" element={<Register/>}/> */}
+    <Route/>
+    <Route/>
+  </Routes>
+  </Router>
+  </>
+  )
 }
 
 export default App;
