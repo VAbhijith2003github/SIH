1a5d405e0c8f51664e8d334c40008b62bac95ddf
V Abhijith <119759593+VAbhijith2003github@users.noreply.github.com>
2023-12-04 02:02:45 +0530

    Fix: responsiveness of the background image, navbar anchor tag elements, aboutus section

--- src/components/reusablecomponents/about.jsx
+++ src/components/reusablecomponents/about.jsx
@@ -1,37 +1,47 @@
-import Temp_logo from "../../images/temp_logo.webp";
-import "./about.css";
+import logo1 from "../../images/logo.webp";
+import logo2 from "../../images/logosih.webp";
+import logo3 from "../../images/logoteam.jpeg";
+import "../../styles.css";
 const About = () => {
-    return (
+  return (
+    <div className="bg">
+      <div className="aboutus">
+        <h1>About Us </h1>
+      </div>
+      <div className="container-fluid">
+        <div className="row aboutrow">
+          <div className="col a col-md-3 col-12">
+            <img src={logo1} alt="Government logo" className="logos" />
+            <h2>Govt of Jharkhand</h2>
+            <p> Administrative authority of Sahyatri</p>
+          </div>
 
-        <div className="bg">
-            <div className="temp0">
-                <h1>About Us </h1>
-            </div>
-            <div className="container-fluid temp1">
-                <div className="row temp2">
-                    <div className="col a col-md-3 col-12">
-                        <img src={Temp_logo} alt="temp logo" className="img-fluid rounded-circle logo"/>
-                        <h2>Govt of jharkhand</h2>
-                        <p>Text goes here </p>
-                    </div>
+          <div className="col a col-md-3 col-12">
+            <img
+              src={logo2}
+              alt="SIH logo"
+              className="logos"
+              style={{ height: "120px"}  
+            }
+            />
+            <h2>SIH</h2>
+            <p>Development Event Host</p>
+          </div>
 
-                    <div className="col a col-md-3 col-12">
-                        <img src={Temp_logo} alt="temp logo"  className="img-fluid rounded-circle logo" />
-                        <h2>Govt of jharkhand</h2>
-                        <p>Text goes here </p>
-                    </div>
-
-                    <div className="col a col-md-3 col-12">
-                        <img src={Temp_logo} alt="temp logo"  className="img-fluid rounded-circle logo" />
-                        <h2>Govt of jharkhand</h2>
-                        <p>Text goes here </p>
-                    </div>
-                </div>
-            </div>
-
+          <div className="col a col-md-3 col-12">
+            <img
+              src={logo3}
+              alt="The Code Crusaders"
+              className="logos"
+              style={{ borderRadius: "50%", height: "150px" }}
+            />
+            <h2>The Code Crusaders</h2>
+            <p>Development partner</p>
+          </div>
         </div>
-
-   )
-}
+      </div>
+    </div>
+  );
+};
 
 export default About;
